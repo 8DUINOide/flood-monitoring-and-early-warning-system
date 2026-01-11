ef614553f02310e0a33f8149bdcd7ba2e41b5152
Al Francis B. Paz <afpaz@gbox.adnu.edu.ph>
2026-01-12 06:46:13 +0800

    change URL

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js - Load environment variables
 const CONFIG = {
     MAPBOX_TOKEN: 'pk.eyJ1IjoiYWxmcmFuY2lzYnA0IiwiYSI6ImNtajloOW4zYzBjYTAzZHNiaHVuc2V1dWUifQ.m_UdZu36KHAKXu8-3TXElQ',
-    DTM_TILE_URL: 'https://earthengine.googleapis.com/v1/projects/floodmonitor-482303/maps/74a756330b2519764939472db2bafee4-6528ca5dedfa9f85c137d5120e488398/tiles/{z}/{x}/{y}',
-    SLOPE_TILE_URL: 'https://earthengine.googleapis.com/v1/projects/floodmonitor-482303/maps/e11f538e813eea5a699de452b6e14c9f-f0fa16ed35c87819de11372b75da33fb/tiles/{z}/{x}/{y}'
+    DTM_TILE_URL: 'https://earthengine.googleapis.com/v1/projects/floodmonitor-482303/maps/74a756330b2519764939472db2bafee4-8702c3a7e9b58830bd6966bacbb7a6d1/tiles/{z}/{x}/{y}',
+    SLOPE_TILE_URL: 'https://earthengine.googleapis.com/v1/projects/floodmonitor-482303/maps/e11f538e813eea5a699de452b6e14c9f-66c130c66ea79094a56a11ed01e278aa/tiles/{z}/{x}/{y}'
 };
 
 window.CONFIG = CONFIG;
